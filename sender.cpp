40fbe284f657a4348e857d94637f0228fbfb5469
Sam <sylnnnn@gmail.com>
2020-05-01 19:00:13 +0800

    Create sender.cpp

---
+++ sender.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+    return r;
+}
+
+int main()
+{
+    std::string s = "A";
+    for (int i = 0; i < 32365; i++)
+        s += "A";
+    s += "SECRET";
+    s = "eater.exe " + s;
+    std::wstring stemp = s2ws(s);
+
+    STARTUPINFO info = { sizeof(info) };
+    PROCESS_INFORMATION processInfo;
+    if (CreateProcess(NULL, (LPWSTR)stemp.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+    {
+        WaitForSingleObject(processInfo.hProcess, INFINITE);
+        CloseHandle(processInfo.hProcess);
+        CloseHandle(processInfo.hThread);
+    }
+}
